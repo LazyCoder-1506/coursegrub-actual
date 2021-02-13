a5607f79a40688fffd8963e14371f629628cb9ef
Pranayan Metiya <pranayan1506@gmail.com>
2021-02-13 17:21:21 +0530

    ready for trying dynamic sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Tabs, Tab } from '@material-ui/core'
+import { Tabs, Tab, Box, Typography, Grid } from '@material-ui/core'
 
 export default function SideBar() {
   const [value, setValue] = React.useState(0)
@@ -18,8 +18,52 @@ export default function SideBar() {
       </Tabs>
       <TabPanel
         value={value}
-        index={0}>
-        Course Details
+        index={0}
+        style={{padding : '16px'}}>
+        <Box
+          component={ Grid }
+          container>
+          <Box
+            component={Grid}
+            item
+            xs={12}
+            my={5}>
+            <Typography
+              variant='h4'>
+              Python for Everybody
+            </Typography>
+          </Box>
+          <Box
+            component={Grid}
+            item
+            xs={12}
+            my={5}>
+            <Typography
+              variant='h5'>
+              Charles Severance - University of Michigan
+            </Typography>
+          </Box>
+          <Box
+            component={Grid}
+            item
+            xs={12}
+            mb={5}>
+            <Typography
+              variant='h5'>
+              Coursera
+            </Typography>
+          </Box>
+          <Box
+            component={Grid}
+            item
+            xs={12}
+            mb={1}>
+            <Typography
+              variant='body1'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+            </Typography>
+          </Box>
+        </Box>
       </TabPanel>
       <TabPanel
         value={value}
