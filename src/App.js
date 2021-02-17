f922d148aee4a942f2cba9082683ded2dda4347f
Pranayan Metiya <pranayan1506@gmail.com>
2021-02-17 17:56:19 +0530

    added dummy navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import Content from './components/Content'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <Grid container direction='column'>
-      <Grid item container>
-        <Grid item xs={false} lg={2}></Grid>
-        <Grid item xs={false} lg={2}></Grid>
-        <Grid item xs={12} lg={6}>
-          <Content></Content>
+    <React.Fragment>
+      <Navbar />
+      <Grid container direction='column'>
+        <Grid item container>
+          <Grid item xs={false} lg={2}></Grid>
+          <Grid item xs={false} lg={2}></Grid>
+          <Grid item xs={12} lg={6}>
+            <Content></Content>
+          </Grid>
+          <Grid item xs={false} lg={2}></Grid>
         </Grid>
-        <Grid item xs={false} lg={2}></Grid>
       </Grid>
-    </Grid>
+    </React.Fragment>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core'
+
+export default function Navbar () {
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <Typography
+          variant='h6'>
+          CourseGrub
+        </Typography>
+        <Button
+          variant='contained'
+          color='primary'
+          disableElevation='true'>
+          Home
+        </Button>
+        <Button
+          variant='contained'
+          color='primary'
+          disableElevation='true'>
+          Courses
+        </Button>
+        <Button
+          variant='contained'
+          color='primary'
+          disableElevation='true'>
+          Contact
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
